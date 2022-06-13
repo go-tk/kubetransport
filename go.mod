84cc4593f17a3692058a7794eefea5a567d1a851
Roy O'Young <roy2220@outlook.com>
2022-06-14 00:38:52 +0800

    Add k8s client

--- go.mod
+++ go.mod
@@ -3,45 +3,66 @@ module github.com/go-tk/kubetransport
 go 1.17
 
 require (
+	github.com/adammck/venv v0.0.0-20200610172036-e77789703e7c
+	github.com/benbjohnson/clock v1.3.0
 	github.com/go-tk/testcase v0.6.2
+	github.com/go-tk/vk8s v0.2.0
+	github.com/spf13/afero v1.8.2
 	github.com/stretchr/testify v1.7.0
 	github.com/valyala/fastrand v1.1.0
-	k8s.io/api v0.23.1
-	k8s.io/apimachinery v0.23.1
-	k8s.io/client-go v0.23.1
+	k8s.io/api v0.24.2
+	k8s.io/apimachinery v0.24.2
+	k8s.io/client-go v0.24.2
 )
 
 require (
+	github.com/Azure/go-ansiterm v0.0.0-20210617225240-d185dfc1b5a1 // indirect
+	github.com/Microsoft/go-winio v0.5.2 // indirect
+	github.com/PuerkitoBio/purell v1.1.1 // indirect
+	github.com/PuerkitoBio/urlesc v0.0.0-20170810143723-de5bf2ad4578 // indirect
 	github.com/davecgh/go-spew v1.1.1 // indirect
+	github.com/docker/distribution v2.8.1+incompatible // indirect
+	github.com/docker/docker v20.10.17+incompatible // indirect
+	github.com/docker/go-connections v0.4.0 // indirect
+	github.com/docker/go-units v0.4.0 // indirect
+	github.com/emicklei/go-restful v2.9.5+incompatible // indirect
 	github.com/evanphx/json-patch v4.12.0+incompatible // indirect
 	github.com/go-logr/logr v1.2.0 // indirect
+	github.com/go-openapi/jsonpointer v0.19.5 // indirect
+	github.com/go-openapi/jsonreference v0.19.5 // indirect
+	github.com/go-openapi/swag v0.19.14 // indirect
 	github.com/gogo/protobuf v1.3.2 // indirect
 	github.com/golang/protobuf v1.5.2 // indirect
-	github.com/google/go-cmp v0.5.5 // indirect
+	github.com/google/gnostic v0.5.7-v3refs // indirect
 	github.com/google/gofuzz v1.1.0 // indirect
-	github.com/googleapis/gnostic v0.5.5 // indirect
 	github.com/imdario/mergo v0.3.5 // indirect
+	github.com/josharian/intern v1.0.0 // indirect
 	github.com/json-iterator/go v1.1.12 // indirect
+	github.com/mailru/easyjson v0.7.6 // indirect
 	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
 	github.com/modern-go/reflect2 v1.0.2 // indirect
+	github.com/munnerz/goautoneg v0.0.0-20191010083416-a7dc8b61c822 // indirect
+	github.com/opencontainers/go-digest v1.0.0 // indirect
+	github.com/opencontainers/image-spec v1.0.2 // indirect
 	github.com/pkg/errors v0.9.1 // indirect
 	github.com/pmezard/go-difflib v1.0.0 // indirect
+	github.com/sirupsen/logrus v1.8.1 // indirect
 	github.com/spf13/pflag v1.0.5 // indirect
-	golang.org/x/net v0.0.0-20211209124913-491a49abca63 // indirect
-	golang.org/x/oauth2 v0.0.0-20210819190943-2bc19b11175f // indirect
-	golang.org/x/sys v0.0.0-20210831042530-f4d43177bf5e // indirect
-	golang.org/x/term v0.0.0-20210615171337-6886f2dfbf5b // indirect
+	golang.org/x/net v0.0.0-20220127200216-cd36cc0744dd // indirect
+	golang.org/x/oauth2 v0.0.0-20211104180415-d3ed0bb246c8 // indirect
+	golang.org/x/sys v0.0.0-20220209214540-3681064d5158 // indirect
+	golang.org/x/term v0.0.0-20210927222741-03fcf44c2211 // indirect
 	golang.org/x/text v0.3.7 // indirect
-	golang.org/x/time v0.0.0-20210723032227-1f47c861a9ac // indirect
+	golang.org/x/time v0.0.0-20220609170525-579cf78fd858 // indirect
 	google.golang.org/appengine v1.6.7 // indirect
 	google.golang.org/protobuf v1.27.1 // indirect
 	gopkg.in/inf.v0 v0.9.1 // indirect
 	gopkg.in/yaml.v2 v2.4.0 // indirect
 	gopkg.in/yaml.v3 v3.0.0-20210107192922-496545a6307b // indirect
-	k8s.io/klog/v2 v2.30.0 // indirect
-	k8s.io/kube-openapi v0.0.0-20211115234752-e816edb12b65 // indirect
-	k8s.io/utils v0.0.0-20210930125809-cb0fa318a74b // indirect
-	sigs.k8s.io/json v0.0.0-20211020170558-c049b76a60c6 // indirect
-	sigs.k8s.io/structured-merge-diff/v4 v4.1.2 // indirect
+	k8s.io/klog/v2 v2.60.1 // indirect
+	k8s.io/kube-openapi v0.0.0-20220328201542-3ee0da9b0b42 // indirect
+	k8s.io/utils v0.0.0-20220210201930-3a6ce19ff2f9 // indirect
+	sigs.k8s.io/json v0.0.0-20211208200746-9f7c6b3444d2 // indirect
+	sigs.k8s.io/structured-merge-diff/v4 v4.2.1 // indirect
 	sigs.k8s.io/yaml v1.2.0 // indirect
 )
---
+++ internal/k8sclient/k8sclient.go
@@ -0,0 +1,320 @@
+package k8sclient
+
+import (
+	"context"
+	"crypto/tls"
+	"crypto/x509"
+	"encoding/json"
+	"errors"
+	"fmt"
+	"net"
+	"net/http"
+	"sync"
+	"sync/atomic"
+	"time"
+	"unsafe"
+
+	"github.com/adammck/venv"
+	"github.com/benbjohnson/clock"
+	"github.com/spf13/afero"
+)
+
+type K8sClient interface {
+	GetEndpoints(ctx context.Context, namespace, endpointsName string) (endpoints *Endpoints, err error)
+	WatchEndpoints(ctx context.Context, namespace, endpointsName, resourceVersion string, callback WatchEndpointsCallback) (err error)
+}
+
+type Metadata struct {
+	ResourceVersion string `json:"resourceVersion"`
+}
+
+type Endpoints struct {
+	Metadata Metadata         `json:"metadata"`
+	Subsets  []EndpointSubset `json:"subsets"`
+}
+
+type EndpointSubset struct {
+	Addresses []EndpointAddress `json:"addresses"`
+}
+
+type EndpointAddress struct {
+	IP string `json:"ip"`
+}
+
+type EventType string
+
+const (
+	EventAdded    EventType = "ADDED"
+	EventModified EventType = "MODIFIED"
+	EventDeleted  EventType = "DELETED"
+
+	eventError EventType = "ERROR"
+)
+
+type WatchEndpointsCallback func(eventType EventType, endpoints *Endpoints) (ok bool)
+
+func New() (K8sClient, error) { return doNew(afero.NewOsFs(), venv.OS(), clock.New()) }
+
+func doNew(fs afero.Fs, env venv.Env, clock clock.Clock) (K8sClient, error) {
+	var kc k8sClient
+	if err := kc.Init(fs, env, clock); err != nil {
+		return nil, err
+	}
+	return &kc, nil
+}
+
+const (
+	serviceHostEnvVarName = "KUBERNETES_SERVICE_HOST"
+	servicePortEnvVarName = "KUBERNETES_SERVICE_PORT"
+	tokenFilePath         = "/var/run/secrets/kubernetes.io/serviceaccount/token"
+	caCertFilePath        = "/var/run/secrets/kubernetes.io/serviceaccount/ca.crt"
+	namespaceFilePath     = "/var/run/secrets/kubernetes.io/serviceaccount/namespace"
+)
+
+const tokenRefreshInterval = 1 * time.Minute
+
+type k8sClient struct {
+	fs              afero.Fs
+	env             venv.Env
+	clock           clock.Clock
+	http            http.Client
+	serviceHostPort string
+	namespace       string
+	token           token
+}
+
+func (kc *k8sClient) Init(fs afero.Fs, env venv.Env, clock clock.Clock) error {
+	kc.fs = fs
+	kc.env = env
+	kc.clock = clock
+	var err error
+	kc.http.Transport, err = kc.makeHTTPTransport()
+	if err != nil {
+		return err
+	}
+	kc.serviceHostPort, err = kc.getServiceHostPort()
+	if err != nil {
+		return err
+	}
+	kc.namespace, err = kc.getNamespace()
+	if err != nil {
+		return err
+	}
+	if _, err := kc.token.Get(clock, fs); err != nil {
+		return err
+	}
+	return nil
+}
+
+func (kc *k8sClient) makeHTTPTransport() (*http.Transport, error) {
+	certPool := x509.NewCertPool()
+	caCertData, err := afero.ReadFile(kc.fs, caCertFilePath)
+	if err != nil {
+		return nil, fmt.Errorf("read ca certificate file; filePath=%q: %w", caCertFilePath, err)
+	}
+	if !certPool.AppendCertsFromPEM(caCertData) {
+		return nil, errors.New("can't add ca certificate")
+	}
+	return &http.Transport{
+		TLSClientConfig: &tls.Config{
+			MinVersion: tls.VersionTLS12,
+			RootCAs:    certPool,
+		},
+	}, nil
+}
+
+func (kc *k8sClient) getServiceHostPort() (string, error) {
+	serviceHost, ok := kc.env.LookupEnv(serviceHostEnvVarName)
+	if !ok {
+		return "", errors.New("can't find environment variable " + serviceHostEnvVarName)
+	}
+	servicePort, ok := kc.env.LookupEnv(servicePortEnvVarName)
+	if !ok {
+		return "", errors.New("can't find environment variable " + servicePortEnvVarName)
+	}
+	return net.JoinHostPort(serviceHost, servicePort), nil
+}
+
+func (kc *k8sClient) getNamespace() (string, error) {
+	namespaceData, err := afero.ReadFile(kc.fs, namespaceFilePath)
+	if err != nil {
+		return "", fmt.Errorf("read namespace file; filePath=%q: %w", namespaceFilePath, err)
+	}
+	return string(namespaceData), nil
+}
+
+func (kc *k8sClient) makeHTTPHeader() (http.Header, error) {
+	tokenData, err := afero.ReadFile(kc.fs, tokenFilePath)
+	if err != nil {
+		return nil, fmt.Errorf("read token file; filePath=%q: %w", tokenFilePath, err)
+	}
+	return http.Header{
+		"Authorization": []string{"Bearer " + string(tokenData)},
+	}, nil
+}
+
+func (kc *k8sClient) GetEndpoints(ctx context.Context, namespace, endpointsName string) (*Endpoints, error) {
+	url := kc.makeURL("/api/v1/namespaces/%s/endpoints/%s", namespace, endpointsName)
+	httpResponse, err := kc.doHTTPGetRequest(ctx, url)
+	if err != nil {
+		return nil, err
+	}
+	defer httpResponse.Body.Close()
+	if httpResponse.StatusCode != http.StatusOK {
+		if httpResponse.StatusCode == http.StatusNotFound {
+			return nil, nil
+		}
+		return nil, fmt.Errorf("http get %q; statusCode=%v", url, httpResponse.StatusCode)
+	}
+	var endpoints Endpoints
+	if err := json.NewDecoder(httpResponse.Body).Decode(&endpoints); err != nil {
+		return nil, fmt.Errorf("decode endpoints json: %w", err)
+	}
+	return &endpoints, nil
+}
+
+func (kc *k8sClient) WatchEndpoints(ctx context.Context, namespace, endpointsName, resourceVersion string, callback WatchEndpointsCallback) error {
+	err := kc.doWatchEndpoints(ctx, namespace, endpointsName, resourceVersion, callback)
+	if status, ok := err.(*status); ok && resourceVersion != "" && status.Code == http.StatusGone {
+		err = kc.doWatchEndpoints(ctx, namespace, endpointsName, "", func(eventType EventType, endpoints *Endpoints) bool {
+			if eventType == EventAdded && endpoints.Metadata.ResourceVersion == resourceVersion {
+				return true
+			}
+			return callback(eventType, endpoints)
+		})
+	}
+	return err
+}
+
+func (kc *k8sClient) doWatchEndpoints(ctx context.Context, namespace, endpointsName, resourceVersion string, callback WatchEndpointsCallback) error {
+	url := kc.makeURL("/api/v1/watch/namespaces/%s/endpoints/%s", namespace, endpointsName)
+	if resourceVersion != "" {
+		url += "?resourceVersion=" + resourceVersion
+	}
+	httpResponse, err := kc.doHTTPGetRequest(ctx, url)
+	if err != nil {
+		return err
+	}
+	defer httpResponse.Body.Close()
+	if httpResponse.StatusCode != http.StatusOK {
+		return fmt.Errorf("http get %q; statusCode=%v", url, httpResponse.StatusCode)
+	}
+	decoder := json.NewDecoder(httpResponse.Body)
+	for {
+		var endpoints *Endpoints
+		event := event{
+			Object: &endpoints,
+		}
+		if err := decoder.Decode(&event); err != nil {
+			return fmt.Errorf("decode event json: %w", err)
+		}
+		if event.Type == eventError {
+			return event.Object.(*status)
+		}
+		if !callback(event.Type, endpoints) {
+			return nil
+		}
+	}
+}
+
+func (kc *k8sClient) makeURL(urlPathTemplate, namespace, resourceName string) string {
+	if namespace == "" {
+		namespace = kc.namespace
+	}
+	return fmt.Sprintf("https://%s"+urlPathTemplate, kc.serviceHostPort, namespace, resourceName)
+}
+
+func (kc *k8sClient) doHTTPGetRequest(ctx context.Context, url string) (*http.Response, error) {
+	httpRequest, err := http.NewRequestWithContext(ctx, "GET", url, nil)
+	if err != nil {
+		return nil, fmt.Errorf("new get request; url=%q: %w", url, err)
+	}
+	token, err := kc.token.Get(kc.clock, kc.fs)
+	if err != nil {
+		return nil, fmt.Errorf("get token; %w", err)
+	}
+	httpRequest.Header["Authorization"] = []string{"Bearer " + token}
+	httpResponse, err := kc.http.Do(httpRequest)
+	if err != nil {
+		return nil, fmt.Errorf("http get %q: %w", url, err)
+	}
+	if httpResponse.StatusCode == http.StatusUnauthorized {
+		kc.token.Reset()
+	}
+	return httpResponse, nil
+}
+
+type token struct {
+	lock   sync.Mutex
+	state1 unsafe.Pointer
+}
+
+type tokenState struct {
+	Value           string
+	NextRefreshTime time.Time
+}
+
+func (t *token) Get(clock clock.Clock, fs afero.Fs) (string, error) {
+	now := clock.Now()
+	if state := t.state(); state != nil && state.NextRefreshTime.After(now) {
+		return state.Value, nil
+	}
+	t.lock.Lock()
+	defer t.lock.Unlock()
+	if state := t.state(); state != nil && state.NextRefreshTime.After(now) {
+		return state.Value, nil
+	}
+	state, err := t.newState(fs, now)
+	if err != nil {
+		return "", err
+	}
+	t.setState(state)
+	return state.Value, nil
+}
+
+func (t *token) Reset() { t.setState(nil) }
+
+func (t *token) newState(fs afero.Fs, now time.Time) (*tokenState, error) {
+	tokenData, err := afero.ReadFile(fs, tokenFilePath)
+	if err != nil {
+		return nil, fmt.Errorf("read token file; filePath=%q: %w", tokenFilePath, err)
+	}
+	return &tokenState{
+		Value:           string(tokenData),
+		NextRefreshTime: now.Add(tokenRefreshInterval),
+	}, nil
+}
+
+func (t *token) state() *tokenState         { return (*tokenState)(atomic.LoadPointer(&t.state1)) }
+func (t *token) setState(state *tokenState) { atomic.StorePointer(&t.state1, unsafe.Pointer(state)) }
+
+type event struct {
+	Type   EventType
+	Object interface{}
+}
+
+var _ json.Unmarshaler = (*event)(nil)
+
+type status struct {
+	Code    int    `json:"code"`
+	Message string `json:"message"`
+}
+
+var _ error = (*status)(nil)
+
+func (s *status) Error() string { return s.Message }
+
+func (e *event) UnmarshalJSON(data []byte) error {
+	rawEvent := struct {
+		Type   EventType       `json:"type"`
+		Object json.RawMessage `json:"object"`
+	}{}
+	if err := json.Unmarshal(data, &rawEvent); err != nil {
+		return err
+	}
+	e.Type = rawEvent.Type
+	if e.Type == eventError {
+		e.Object = &status{}
+	}
+	return json.Unmarshal(rawEvent.Object, e.Object)
+}
